8afcb557a500ddb4ff7bf26bd921af1c17162088
Michael Balogh <mjbalogh@fullsail.edu>
2012-04-04 23:57:25 -0500

    Added classes for player, weapon, and zombie. Added log function to Object for debugging. Improved logging on trapped errors.

--- Balogh_Michael_assignment2.js
+++ Balogh_Michael_assignment2.js
@@ -1,42 +1,98 @@
-// setup namespace
+/*
+	Project 2
+	Scalable Data Infrastructures
+	Author: Michael Balogh
+	Date: 04/04/2012
+	Note: Continuation of Zombie story from 1st assignment
+*/
+
+// define namespace and protect undefined
 (function(ns, undefined) {
 	// private variables
-	// var varname = value;
+	var _debug = true,
+		maxFamily = 4,
+		maxZombiesPerPerson = 4;
 	
 	// private functions
-	// var funcname = function(arguments) {};
+	// var funcname = function (parameters) {};
+	Object.log = function (object) {
+		for (var i in object) {
+			if (object.hasOwnProperty(i) && (typeof(object[i]) !== 'function')) {
+				console.log(i, ":", object[i]);
+			}
+		}
+	};
 	
 	// public variables
-	// ns.varname = value;
+	ns.letsKillZombies = true,
+	ns.numZombies = Math.floor(Math.random() * (ns.totalFamily * Math.floor(Math.random() * (maxZombiesPerPerson /* max per (4) - min per (1) + 1 */)))) + 1;
+	
+	ns.WEAPON_TYPE = Object.freeze({
+		cricketbat : {name : "cricket bat"},
+		revolver : {name : "revolver", maxAmmo: 6}
+	});
+	ns.DAMAGE_LOCATION = Object.freeze({
+		miss : 0,
+		body : 1,
+		head : 2
+	});
 	
 	// public functions
-	// ns.funcname = function(arguments) {};
-} (window.sdi = window.sdi || {})); // define namespace and protect undefined (by not passing it a value)
-
-// global workings. Call our stuff by namespace
-try {
+	ns.player = function (name, numFamily) {
+		this.name = name,
+		this.numFamily = numFamily || Math.floor(Math.random() * maxFamily /* maxFamily - player + 1 */);
+		
+		if (_debug) {
+			console.log("*** PLAYER ***");
+			Object.log(this);
+		}
+	};
 	
+	ns.weapon = function (type, functional) {
+		switch (type) {
+			case ns.WEAPON_TYPE.cricketbat:
+				this.name = ns.WEAPON_TYPE.cricketbat.name;
+				break;
+			case ns.WEAPON_TYPE.revolver:
+				this.name = ns.WEAPON_TYPE.revolver.name;
+				this.maxAmmo = ns.WEAPON_TYPE.revolver.maxAmmo;
+				this.currentAmmo = Math.floor(Math.random() * this.maxAmmo /* maxAmmo - 1 min + 1 */) + 1;
+				break;
+			default:
+				throw new Error("Undefined weapon is undefined!")
+				break;
+		}
+		this.functional = functional;
+		
+		this.log = function() {
+			for (var i in this) {
+				if (this.hasOwnProperty(i) && typeof(this[i]) !== 'function') {
+					console.log(i, ":", this[i]);
+				}
+			}
+		};
+		if (_debug) {
+			console.log("*** WEAPON ***");
+			Object.log(this);
+		}
+	};
+
+	ns.zombie = function() {
+		this.killed = false;
+	};
+} (window.sdi = window.sdi || {})); // define namespace and protect undefined
+
+try {
+	var player = new sdi.player("Shawn"),
+	var revolver = new sdi.weapon(sdi.WEAPON_TYPE.revolver, true),
+		cricketbat = new sdi.weapon(sdi.WEAPON_TYPE.cricketbat, true),
+		zombies = [];
+	
+	for (var i = 0; i < sdi.numZombies; i++) {
+		zombies.push(new sdi.zombie());
+	}
+	console.log("Number of Zombies:", zombies.length);
 } catch(e) {
-	console.log(e.message);
+	console.log(e.message, "in file:", e.fileName, "on line:", e.lineNumber);
 }
 
-/*
-var tankNames = ["Shark Tank", "Sting Ray Tank", "Dolphin Tank", "Fish Tank"],
-	minutesPerTank = [20, 15, 25, 30],
-	kidNames = ["Braden", "Rhys", "Zoe"];
-
-var visitTank = function(whatTank) {
-    var tankName = tankNames[whatTank],
-    minutesThisTank = minutesPerTank[tankNumber];
-    console.log(kidNames[0] + " wanted us to visit the " + tankName + " for " + minutesThisTank + " minutes. So I set the timer on my watch!");
-    for (var minutes = 0; minutes < minutesThisTank; minutes += 5) {
-            var minutesRemain = minutesThisTank - minutes;
-            console.log("We have " + minutesRemain + " minutes left. " + minutes + " minutes have past.");
-    }
-    console.log("We finished with the " + tankName + ".");
-};
-
-for (var tankNumber = 0; tankNumber < tankNames.length; tankNumber++) {
-    visitTank(tankNumber); 
-};
-*/

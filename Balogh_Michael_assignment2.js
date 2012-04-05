ef88d1b6aabdc72c45c0a613f7047a4788463173
Michael Balogh <mjbalogh@fullsail.edu>
2012-04-05 15:36:16 -0500

    rewrite of js file from scratch. Overcomplicated things for this assignment, and need to meet deadline.

--- Balogh_Michael_assignment2.js
+++ Balogh_Michael_assignment2.js
@@ -5,17 +5,14 @@
 	Date: 04/04/2012
 	Note: Continuation of Zombie story from 1st assignment
 */
-
-// define namespace and protect undefined
-(function(ns, undefined) {
-	// private variables
-	var debug = true,
-		maxFamily = 4,
-		maxAmmo = 99;
-		maxZombiesPerPerson = 4;
+var debug = false,
+	maxFamily = 4,
+	maxAmmo = 99,
+	maxZombiesPerPerson = 4,
+	numZombies = Math.floor(Math.random() * (maxFamily * Math.floor(Math.random() * maxZombiesPerPerson))) + 1
+	zombies = [];
 	
-	// private functions
-	var debugLogObjectProperties = function (object, description) {
+	var debugObjectProperties = function (object, description) {
 		if (debug) {
 			this.description = description || object['name'];
 			console.log("***", this.description.toUpperCase(), "***");
@@ -26,111 +23,120 @@
 			}	
 		}
 	};
-	var getIndexByName = function (array, name) {
-		var len = array.length;
-		for (var i = 0; i < len; i++) {
-			var obj = array[i];
-			if (undefined !== obj.name && name.toUpperCase() === obj.name.toUpperCase()) return i;
-		}
-		return -1;
+	var getRandomBool = function (range, limit) {
+		var cutoff = limit || range / 2;
+		return (Math.floor(Math.random() * 100) >= cutoff ? true : false);
 	};
-		
-	// public variables
-	ns.letsKillZombies = true,
-	ns.numZombies = Math.floor(Math.random() * (maxFamily * Math.floor(Math.random() * maxZombiesPerPerson))) + 1;
-	
-	// public "enums"
-	ns.WEAPON_TYPE = Object.freeze({
-		cricketbat : {name : "cricket bat", type : 'melee'},
-		revolver : {name : "revolver", type : 'ranged', maxAmmo : 6}
-	});
-	
-	ns.DAMAGE_LOCATION = Object.freeze({
-		body : 0,
-		head : 1
-	});
-	
-	// public functions
-	// *** WEAPON ***
-	ns.weapon = function (type, functional) {
-		switch (type) {
-			case ns.WEAPON_TYPE.cricketbat:
-				this.name = type.name;
-				this.range = type.type;
-				break;
-			case ns.WEAPON_TYPE.revolver:
-				this.name = type.name;
-				this.range = type.type;
-				this.maxAmmo = type.maxAmmo;
-				this.currentAmmo = Math.floor(Math.random() * this.maxAmmo) + 1;
-				break;
-			default:
-				throw new Error("Undefined weapon is undefined!")
-				break;
+	var createZombies = function () {
+		for (var i = 0; i < numZombies; i++) {
+			zombies.push('zombie');
 		}
-		this.functional = functional || (Math.floor(Math.random() * Object.keys(ns.WEAPON_TYPE).length) === 1) ? true : false;
-		
-		debugLogObjectProperties(this, 'weapon');
 	};
 	
-	// *** PLAYER ***
-	ns.player = function (name, numFamily) {
-		this.name = name,
-		this.numFamily = numFamily || Math.floor(Math.random() * maxFamily) +  1,
-		this.ammo = Math.floor(Math.random() * maxAmmo) + 1;
-		this.weapons = [];
-		
-		// functions
-		this.loadWeapon = function (name) {
-			var requiredAmmo,
-				index = getIndexByName(this.weapons, name),
-				weapon = this.weapons[index];
-			if (undefined === weapon || weapon.maxAmmo === undefined) return;
-
-			console.log(this.name, "says \"Reloading.\"");
-			requiredAmmo = weapon.maxAmmo - weapon.currentAmmo;
-			this.ammo -= requiredAmmo;
-			weapon.currentAmmo += requiredAmmo;
-		};
-		this.addWeapon = function (weaponType, functional) {
-			var index = getIndexByName(this.weapons, weaponType.name);
-			if (-1 === index) {
-				this.weapons.push(new ns.weapon(weaponType, functional));
-			} else {
-				if (!this.weapons[index].functional) {
-					this.ammo += this.weapons[index].currentAmmo;
-					this.weapons[index] = new ns.weapon(weaponType, functional);
+	var weapon = function (name, functional) {
+		this.name = name;
+		if (name.toUpperCase() === 'REVOLVER') {
+			this.maxAmmo = 6;
+			this.currentAmmo = Math.floor(Math.random() * this.maxAmmo) + 1;
+			this.fireRound = function () {
+				if (this.currentAmmo < 1) {
+					return false;
 				} else {
-					this.weapons.push(new ns.weapon(weaponType, functional));
+					this.currentAmmo--;
+					return true;
+				}
+			};
+			this.reload = function () {
+				var requiredAmmo = this.maxAmmo - this.currentAmmo;
+				if (player.numAmmo < 1) {
+					console.log(player.name, "asks, \"What, no more ammo?\"");
+					return -1;
+				} else {
+					if (requiredAmmo > player.numAmmo && player.numAmmo > 0) {
+						requiredAmmo = player.numAmmo;
+					}
+				}
+				console.log(player.name, "loads", requiredAmmo, "into his", this.name + ".");
+				player.numAmmo -= requiredAmmo;
+				this.currentAmmo += requiredAmmo;
+				return requiredAmmo;
+			};
+		}
+		this.functional = functional || getRandomBool();
+		
+		debugObjectProperties(this, "weapon");
+	};
+	
+	var player = function (name, numFamily) {
+		this.name = name;
+		this.numFamily = numFamily || Math.floor(Math.random() * maxFamily) +  1;
+		this.numAmmo = Math.floor(Math.random() * maxAmmo) + 1;
+		this.rangedWeapon = new weapon('revolver', true);
+		this.meleeWeapon = new weapon('cricket bat', true);
+		
+		this.chooseWeapon = function() {
+			if (this.rangedWeapon.functional) {
+				return this.rangedWeapon;
+			} else if (this.meleeWeapon.functional) {
+				return this.meleeWeapon;
+			} else {
+				console.log(this.name, "look around for another weapon.");
+				return undefined;
+			}
+		};
+		this.loadWeapon = function (speak) {
+			if (undefined === this.rangedWeapon) {
+				if (speak){
+					console.log(this.name, "says, \"All this ammo, and nothing to use it in.\"");
 				}
 			}
-			if ('ranged' === weaponType.type) {
-				this.loadWeapon(weaponType.name);
+			if (speak) {
+				console.log(this.name, "says, \"Reloading.\"");
 			}
 		};
-		this.attack = function (weaponName, zombie) {
-			throw new Error("Function not implemented!");
-		};
-		
-		debugLogObjectProperties(this, 'player');
+		this.attack = function (weapon, zombie) {
+			var success = getRandomBool(),
+				headshot = getRandomBool(),
+				weaponBroken = !getRandomBool(20);
+			if (undefined === weapon || undefined === zombie) return false;
+			if (player.rangedWeapon === weapon) {
+				if (!weapon.fireRound()) {
+					this.loadWeapon();
+					return false;
+				}
+			}
+			if (success) {
+				if (headshot) {
+					console.log(this.name, "shouts, \"Boom, headshot!\"");
+					zombies.shift();
+				} else {
+					console.log(this.name, "moans, \"Awwww... not again.\"");
+				}
+			}
+			if (weaponBroken) {
+				weapon.functional = false;
+				console.log(this.name, "shouts, \"Oh, c'mon!!!\" as his weapon breaks.");
+			}
+			return success;
+		}
+		debugObjectProperties(this, "player");
 	};
-	
-	ns.zombie = function() {
-		this.killed = false;
-	};
-} (window.sdi = window.sdi || {})); // define namespace and protect undefined
 
 try {
-	var player = new sdi.player("Shawn"),
-		zombies = [];
-	player.addWeapon(sdi.WEAPON_TYPE.revolver, true);
-	player.addWeapon(sdi.WEAPON_TYPE.cricketbat, true);
-	
-	for (var i = 0; i < sdi.numZombies; i++) {
-		zombies.push(new sdi.zombie());
+	var player = new player("Shawn");
+	if (player.rangedWeapon.currentAmmo < player.rangedWeapon.maxAmmo) {
+		player.loadWeapon(false);
 	}
-	console.log("Number of Zombies:", zombies.length);
+	createZombies();
+	for (var z in zombies) {
+		var weapon = player.chooseWeapon();
+		if (undefined === weapon) break;		
+		console.log(player.name, "attacks a zombie with his", weapon.name + ".");
+		player.attack(weapon, z);
+	}
+	if (zombies.length > 0) {
+		console.log(player.name, "needs to locate more weapons before he can save his family!!!")
+	}
 } catch(e) {
 	console.log(e.message, "in file:", e.fileName, "on line:", e.lineNumber);
 }
-

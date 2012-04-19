5468458b3e346ca68b692a53bfeafa1befed87b6
Michael Balogh <mjbalogh@fullsail.edu>
2012-04-19 05:09:34 -0500

    Created JSON Data

--- Balogh_Michael_assignment3.js
+++ Balogh_Michael_assignment3.js
@@ -1,1 +1,85 @@
-alert("JavaScript works!");
+/*
+	Project 3
+	Scalable Data Infrastructures
+	Author: Michael Balogh
+	Date: 04/18/2012
+	Note: New story about job search
+*/
+
+// define namespace, $, and protect undefined
+(function(ns, $, undefined){
+	// private
+	var jsonData = $.parse(json);
+	var getIndexByName = function (array, name) {
+		var len = array.length;
+		for (var i = 0; i < len; i++) {
+			var obj = array[i];
+			if (undefined !== obj.name && name.toUpperCase() === obj.name.toUpperCase()) return i;
+		}
+		return -1;
+	};
+
+	// public
+	ns.company = function (name, recruiter, positions) {
+		if (undefined === name || undefined === recruiter) return undefined;
+		this.name = name;
+		this.recruiter = recruiter;
+		this.positions = positions || [];
+		this.getName = function () { return this.name; };
+		this.getRecruiter = function () { return this.recruiter; };
+		this.getPositions = function () { return this.positions; };
+		this.setRecruiter = function (recruiter) {
+			this.recruiter = name;
+		};
+		this.addPosition = function (name, location, salary) {
+			if (undefined === name || undefined === location) return -1;
+			var temp = {
+				name: name,
+				location: location,
+				salary: salary || undefined,
+				have_applied = {},
+				interview = []
+			};
+			this.positions.push(temp);
+		};
+		this.applyForPosition = function (position, cover_letter, resume, application_form) {
+			var n = getIndexByName(this.positions, position),
+				apply = {};
+			if (undefined === position || position === '') return;
+			if (-1 === n) return;
+			
+			apply = {
+				cover_letter: cover_letter || false,
+				resume: resume || false;
+				application_form: application_form || false
+			};
+			this.positions[n].has_applied.push(apply);
+		};
+		this.addInterview = function (position, date, time, interviewer) {
+			var n = getIndexByName(this.positions, position),
+				interview = {};
+			if (undefined === position || undefined === date || undefined === time || undefined === interviewer) return;
+			interview = {
+				date: date,
+				time: time,
+				interviewer: interviewer
+			};
+			this.positions[n].interviews.push(interview);
+		};
+		return {
+			getName: this.getName,
+			getRecruiter: this.getRecruiter,
+			getPositions: this.getPositions,
+			setRecruiter: this.setRecruiter,
+			addPosition: this.addPosition,
+			applyForPosition: this.applyForPosition,
+			addInterview: this.addInterview
+		};
+	};
+	ns.Person = function (name) {
+		if (undefined === name || '' === name) return undefined;
+		this.name = name;
+		this.
+	};
+	
+} (window.sdi = window.sdi || {}, JSON));
---
+++ jsondata.js
@@ -0,0 +1,66 @@
+var json = {
+	"companies": [
+		{
+			"company_name": 'GE',
+			"recruiter": 'James Gun',
+			"positions": [
+				{
+					"name": 'Information Security Governance Analyst',
+					"salary": 80000,
+					"location": 'New Orleans, LA',
+					"have_applied": {
+						"cover_letter": true,
+						"resume": true,
+						"application_form": false
+					},
+					"interviews": []
+				},
+				{
+					"name": 'Information Security Policy Analyst',
+					"salary": 80000,
+					"location": 'New Orleans, LA',
+					"have_applied": {
+						"cover_letter": true,
+						"resume": true,
+						"application_form": false
+					},
+					"interviews": []
+				}
+			]
+		},
+		{
+			"company_name": 'Chevron',
+			"recruiter": 'James Shrieb',
+			"positions": [
+				{
+					"name": 'Senior Network Administrator',
+					"salary": 85280,
+					"location": 'Belle Chasse, LA',
+					"have_applied": {
+						"cover_letter": true,
+						"resume": true,
+						"application_form": false
+					},
+					"interviews": []
+				}
+			],
+		},
+		{
+			"company_name": 'ASI Federal Credit Union',
+			"recruiter": "Julie Schultz",
+			"positions": [
+				{
+					"name": 'Director of Information Security',
+					"salary": 87500,
+					"location": 'Metarie, LA',
+					"have_applied": {
+						"cover_letter": true,
+						"resume": true,
+						"application_form": false
+					},
+					"interviews": []
+				}
+			]
+		}
+	]
+};
